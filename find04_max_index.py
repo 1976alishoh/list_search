89535805e999960c1432a2e6de4bdd642cdff960
1976alishoh <106156697+iqrobocreator@users.noreply.github.com>
2022-11-11 17:58:37 +0300

    04 done

--- find04_max_index.py
+++ find04_max_index.py
@@ -5,4 +5,12 @@ def find_max_index(data):
         data: list of numbers
     returns: index of maximum number in the list
     """
-    return 0
+    idx = 0
+    m = data[idx]
+    for i in range (len(data)):
+        if data[i]>m:
+            m = data[i]
+            idx = i
+    return idx
+print(find_max_index([1, 2, 3, 4, 5]))
+print(find_max_index([6, 8, 7, 4, 0]))
