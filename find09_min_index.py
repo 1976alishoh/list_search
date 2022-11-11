cb6a94d9dda354f3cb0274b4c0edf013707f09a1
1976alishoh <106156697+iqrobocreator@users.noreply.github.com>
2022-11-11 18:47:55 +0300

    09 done

--- find09_min_index.py
+++ find09_min_index.py
@@ -5,5 +5,12 @@ def find_min_index(data):
         data: list of numbers
     returns: index of minimum number in the list
     """
-    return 0
-
+    idx = 0
+    m = data[idx]
+    for i in range (len(data)):
+        if data[i]<m:
+            m = data[i]
+            idx = i
+    return idx
+print(find_min_index([1, 2, -3, 4, 5]))
+print(find_min_index( [12, 2, 5, 2, 7, 9, 1]))
