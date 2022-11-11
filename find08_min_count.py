967ccc77170833def7e452df8246edb4319b2431
1976alishoh <106156697+iqrobocreator@users.noreply.github.com>
2022-11-11 18:45:19 +0300

    08 done

--- find08_min_count.py
+++ find08_min_count.py
@@ -5,4 +5,17 @@ def find_min_count(data):
         data: list of numbers
     returns: count of minimum numbers in the list
     """
-    return 0
+    sanoq = 0
+    min = data[0]
+    for i in data :
+        if i < min :
+            min = i
+    
+    for  i in data :
+        if i == min:
+           sanoq = sanoq + 1
+    return sanoq
+print(find_min_count([1, 8, 3, 8, 5]))
+print(find_min_count([0, -4, 3, 9, -2, -4]))
+
+
