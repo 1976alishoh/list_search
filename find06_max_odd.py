1918ac052ca1b5e54cc1e06a2e817867bd3328ac
1976alishoh <106156697+iqrobocreator@users.noreply.github.com>
2022-11-11 18:34:25 +0300

    06 done

--- find06_max_odd.py
+++ find06_max_odd.py
@@ -5,4 +5,18 @@ def find_max_odd(data):
         data: list of numbers
     returns: maximum odd number in the list
     """
-    return 0
+    odd_num = []
+    for i in data:
+        if i % 2 == 1 and i != 0:
+            odd_num = odd_num + [i]
+    
+    max = odd_num[0]
+    for t in odd_num :
+        if t > max :
+            max = t
+    
+    return max 
+print(find_max_odd([1, 4, 3, 8, 5]))
+print(find_max_odd([7, 6, 3, 4, 9]))
+
+
