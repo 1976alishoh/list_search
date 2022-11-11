b66db67f63a43eb4e4028dc7ae37d1d73ac99e37
1976alishoh <106156697+iqrobocreator@users.noreply.github.com>
2022-11-11 18:43:48 +0300

    07 done

--- find07_min.py
+++ find07_min.py
@@ -5,13 +5,14 @@ def find_min(data):
         data: list of numbers
     returns: minimum number in the list
     """
-
+    sanoq = 0
     min = data[0]
     for i in data :
         if i < min :
             min = i
     
     
-    return  min 
+    
+    
 print(find_min([1, 2, -3, 4, 5]))
 print(find_min([15, 23, 3, 9, 1, 4]))
